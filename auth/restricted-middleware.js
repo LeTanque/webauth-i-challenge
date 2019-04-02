fe31389c3836c6e0458e4fb000715078073e87fd
Frank Martinez <frankie.martinez@gmail.com>
2019-04-02 15:57:08 -0700

    error messaging complies with MVP. Working on stretch goals

--- auth/restricted-middleware.js
+++ auth/restricted-middleware.js
@@ -1,10 +1,10 @@
 module.exports = (req, res, next) => {
     try {
       // if this throws, please don't crash my app
-      if (req && req.session && req.session.user) {
-        next();
+      if (req && req.session && req.session.user) { // Does req.session exist?
+        next(); // If yes, move to next thing. If no, finish out this try/catch pattern
       } else {
-        res.status(401).json({ message: 'Invalid Credentials' });
+        res.status(401).json({ message: 'You shall not pass!' });
       }
     } catch (error) {
       res.status(500).json({ message: 'you broke it!' });
